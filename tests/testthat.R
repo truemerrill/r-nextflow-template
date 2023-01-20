56842f7d9d422bbfbcb1555d2408c8d3b27cb2d0
True Merrill <true.merrill@gtri.gatech.edu>
2023-01-19 21:35:02 -0500

    Fixing import in tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(dockertemplate)
+library(NextflowTemplate)
 
-test_check("dockertemplate")
+test_check("NextflowTemplate")
